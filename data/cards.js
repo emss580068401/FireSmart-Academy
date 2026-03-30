b05e87165a9625911a15bf92329fac4800bb1e61
bluerain710509 <bluerain710509@yahoo.com.tw>
2026-03-30 12:04:38 +0800

    Update: Populate cards.js with custom Stephen Chow movie quotes and descriptions

--- data/cards.js
+++ data/cards.js
@@ -2,26 +2,26 @@ const CARDS_DATA = [
   {
     "id": "01",
     "image": "images/01.jpg",
-    "text": "01",
-    "desc": "說明01"
+    "text": "要爆了",
+    "desc": "醬爆的內心有一種特別的想法"
   },
   {
     "id": "02",
     "image": "images/02.jpg",
-    "text": "02",
-    "desc": "說明02"
+    "text": "投降書一半",
+    "desc": "香港麻將的規則是投降輸一半"
   },
   {
     "id": "03",
     "image": "images/03.jpg",
-    "text": "03",
-    "desc": "說明03"
+    "text": "今晚打老虎",
+    "desc": "法國賭神說給窩擦批寫"
   },
   {
     "id": "04",
     "image": "images/04.jpg",
-    "text": "04",
-    "desc": "說明04"
+    "text": "三角關係他不會",
+    "desc": "B啊，選B啊"
   },
   {
     "id": "05",
@@ -32,151 +32,151 @@ const CARDS_DATA = [
   {
     "id": "06",
     "image": "images/06.jpg",
-    "text": "06",
-    "desc": "說明06"
+    "text": "阿星接力",
+    "desc": "阿星快~接力接力"
   },
   {
     "id": "07",
     "image": "images/07.jpg",
-    "text": "07",
-    "desc": "說明07"
+    "text": "刑事組之虎",
+    "desc": "曹達華"
   },
   {
     "id": "08",
     "image": "images/08.jpg",
-    "text": "08",
-    "desc": "說明08"
+    "text": "我對大人的景仰",
+    "desc": "有如滔滔江水連綿不絕"
   },
   {
     "id": "09",
     "image": "images/09.jpg",
-    "text": "09",
-    "desc": "說明09"
+    "text": "抓奶龍爪手",
+    "desc": "公主~請接招~"
   },
   {
     "id": "10",
     "image": "images/10.jpg",
-    "text": "10",
-    "desc": "說明10"
+    "text": "有沒有搞錯",
+    "desc": "又不是豬，那麼會生"
   },
   {
     "id": "11",
     "image": "images/11.jpg",
-    "text": "11",
-    "desc": "說明11"
+    "text": "燒雞翅",
+    "desc": "他就要歸西"
   },
   {
     "id": "12",
     "image": "images/12.jpg",
-    "text": "12",
-    "desc": "說明12"
+    "text": "兵器譜第一名",
+    "desc": "小李他媽的飛刀"
   },
   {
     "id": "13",
     "image": "images/13.jpg",
-    "text": "13",
-    "desc": "說明13"
+    "text": "旺財你不能死",
+    "desc": "小強~你怎麼了~小強"
   },
   {
     "id": "14",
     "image": "images/14.jpg",
-    "text": "14",
-    "desc": "說明14"
+    "text": "9527",
+    "desc": "你只有代號，沒有姓名"
   },
   {
     "id": "15",
     "image": "images/15.jpg",
-    "text": "15",
-    "desc": "說明15"
+    "text": "小雞吃米圖",
+    "desc": "加個光圈就成鳳凰"
   },
   {
     "id": "16",
     "image": "images/16.jpg",
-    "text": "16",
-    "desc": "說明16"
+    "text": "七省文狀元",
+    "desc": "我兩惺惺相惜情不自禁"
   },
   {
     "id": "17",
     "image": "images/17.jpg",
-    "text": "17",
-    "desc": "說明17"
+    "text": "秋香姊",
+    "desc": "還我漂漂拳"
   },
   {
     "id": "18",
     "image": "images/18.jpg",
-    "text": "18",
-    "desc": "說明18"
+    "text": "小人本住在蘇州的城邊",
+    "desc": "家中有屋又有田"
   },
   {
     "id": "19",
     "image": "images/19.jpg",
-    "text": "19",
-    "desc": "說明19"
+    "text": "含笑半步顛",
+    "desc": "謝謝你 RIP 華夫人"
   },
   {
     "id": "20",
     "image": "images/20.jpg",
-    "text": "20",
-    "desc": "說明20"
+    "text": "我愛黎明",
+    "desc": "張學友 我愛你"
   },
   {
     "id": "21",
     "image": "images/21.jpg",
-    "text": "21",
-    "desc": "說明21"
+    "text": "我家的床",
+    "desc": "又大又舒服"
   },
   {
     "id": "22",
     "image": "images/22.jpg",
-    "text": "22",
-    "desc": "說明22"
+    "text": "大師兄",
+    "desc": "在座的各位都是垃圾"
   },
   {
     "id": "23",
     "image": "images/23.jpg",
-    "text": "23",
-    "desc": "說明23"
+    "text": "奔來手文泰來",
+    "desc": "你是不是不把我放在眼裡"
   },
   {
     "id": "24",
     "image": "images/24.jpg",
-    "text": "24",
-    "desc": "說明24"
+    "text": "無敵風火輪",
+    "desc": "人人有功練"
   },
   {
     "id": "25",
     "image": "images/25.jpg",
-    "text": "25",
-    "desc": "說明25"
+    "text": "豹頭",
+    "desc": "他全都要"
   },
   {
     "id": "26",
     "image": "images/26.jpg",
-    "text": "26",
-    "desc": "說明26"
+    "text": "好大的官威",
+    "desc": "陰陽人 爛屁股"
   },
   {
     "id": "27",
     "image": "images/27.jpg",
-    "text": "27",
-    "desc": "說明27"
+    "text": "常威說他不會武功",
+    "desc": "我是天生神力"
   },
   {
     "id": "28",
     "image": "images/28.jpg",
-    "text": "28",
-    "desc": "說明28"
+    "text": "牛夫人",
+    "desc": "叫她小甜甜"
   },
   {
     "id": "29",
     "image": "images/29.jpg",
-    "text": "29",
-    "desc": "說明29"
+    "text": "ONLY YOU",
+    "desc": "歐你媽個頭"
   },
   {
     "id": "30",
     "image": "images/30.jpg",
-    "text": "30",
-    "desc": "說明30"
+    "text": "曾經有一份真誠的愛情",
+    "desc": "我的答案是 一萬年"
   }
 ];
